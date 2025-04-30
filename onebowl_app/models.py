1f30db090bb1a7ac1bc1a7e2676d5d86f6c1ae92
Reema Radi <reema1radi1@outlook.com>
2025-04-30 10:46:13 +0300

    add recipe model

--- onebowl_app/models.py
+++ onebowl_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Recipe(models.Model):
+    recipe_name = models.CharField(max_length=255)
+    description = models.CharField(max_length=1000)
+    instructions = models.CharField
+    
+    def __str__(self):
+        return self.recipe_name 
+    
+    
+
