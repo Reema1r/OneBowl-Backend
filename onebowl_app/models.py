7485c29b2bd30a26daf1abfb72447be69ed07643
Reema Radi <reema1radi1@outlook.com>
2025-04-30 11:07:22 +0300

    add recipe serializer

--- onebowl_app/models.py
+++ onebowl_app/models.py
@@ -6,7 +6,7 @@ class Recipe(models.Model):
     recipe_name = models.CharField(max_length=255)
     description = models.CharField(max_length=1000)
     instructions = models.TextField()
-    image_url = models.CharField(max_length=255)
+    # image_url = models.CharField(max_length=255)
     
     def __str__(self):
         return self.recipe_name 
