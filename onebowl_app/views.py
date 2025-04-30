c89b2cd2399d7023a851bdad2281434b970b896d
Reema Radi <reema1radi1@outlook.com>
2025-04-30 12:20:24 +0300

    create RecipeListCreateView  and add the url to project urls

--- onebowl_app/views.py
+++ onebowl_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Recipe
+from .serializers import RecipeSerializer
+
 # Create your views here.
+class RecipeListCreateView(APIView):
+    def get(self, request):
+        recipes = Recipe.objects.all()
+        serializer=RecipeSerializer(recipes, many=True)
+        return Response(serializer.data, status=200)
--- onebowl_project/settings.py
+++ onebowl_project/settings.py
@@ -34,6 +34,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
+    'rest_framework',
     'onebowl_app',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -44,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,3 +132,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOW_ALL_ORIGINS = True
