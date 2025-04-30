2441529f9acbc4ebdfd54f10c67babee1f6ebde5
Reema Radi <reema1radi1@outlook.com>
2025-04-30 10:57:11 +0300

    edit recipe model

--- onebowl_app/models.py
+++ onebowl_app/models.py
@@ -5,7 +5,8 @@ from django.db import models
 class Recipe(models.Model):
     recipe_name = models.CharField(max_length=255)
     description = models.CharField(max_length=1000)
-    instructions = models.CharField
+    instructions = models.TextField()
+    image_url = models.CharField(max_length=255)
     
     def __str__(self):
         return self.recipe_name 
