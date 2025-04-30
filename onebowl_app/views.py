12f9b9eee6ef509a2b0acf867dca1d015126fb43
Reema Radi <reema1radi1@outlook.com>
2025-04-30 13:52:47 +0300

    add a post request to RecipeListCreateView

--- onebowl_app/views.py
+++ onebowl_app/views.py
@@ -11,3 +11,12 @@ class RecipeListCreateView(APIView):
         recipes = Recipe.objects.all()
         serializer=RecipeSerializer(recipes, many=True)
         return Response(serializer.data, status=200)
+    
+    def post(self,request):
+        serializer=RecipeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=201)
+            
+        return Response(serializer.errors,status=400)
+    
