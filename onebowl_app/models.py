052a09ef1a3a90f819da50b3ed8ca2d54ddfff36
Reema Radi <reema1radi1@outlook.com>
2025-05-02 14:13:21 +0300

    add ingredient model

--- onebowl_app/models.py
+++ onebowl_app/models.py
@@ -2,14 +2,28 @@ from django.db import models
 
 # Create your models here.
 
+
 class Recipe(models.Model):
-    recipe_name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     description = models.CharField(max_length=1000)
     instructions = models.TextField()
-    img_url = models.CharField(max_length=255)
+    img_url = models.CharField(max_length=400)
     
     def __str__(self):
-        return self.recipe_name 
+        return self.name 
+    
+    
+class Ingredient(models.Model):
+    name=models.CharField(max_length=50)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+    
+    
+    
+
+    
     
     
 
--- onebowl_app/serializers.py
+++ onebowl_app/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
-from .models import Recipe
+from .models import Recipe, Ingredient
 
 class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model=Recipe
         fields="__all__"
+        
+        
