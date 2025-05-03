affc6a5e8bcab9fa30fe2a8fb928662bdac7c594
Reema Radi <reema1radi1@outlook.com>
2025-05-03 18:45:09 +0300

    added post request for shopping list

--- onebowl_app/views.py
+++ onebowl_app/views.py
@@ -70,3 +70,11 @@ class ShoppingListCreateList(APIView):
         serializer=ShoppingListSerializer(shoppingLists, many=True)
         return Response(serializer.data, status=200)
     
+    def post(self,request):
+        serializer=ShoppingListSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=201)
+            
+        return Response(serializer.errors,status=400)
+    
