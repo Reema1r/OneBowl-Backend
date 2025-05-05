ce54c1ade4e57ffe703300ebabc027f56db1863c
Reema Radi <reema1radi1@outlook.com>
2025-05-05 17:22:43 +0300

    added user filed in recipe model, edited recipe serializer and url.py

--- onebowl_app/models.py
+++ onebowl_app/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
-# Create your models here.
-
+from django.contrib.auth.models import User
 
 class Recipe(models.Model):
     name = models.CharField(max_length=255)
@@ -9,6 +8,7 @@ class Recipe(models.Model):
     instructions = models.TextField()
     img_url = models.CharField(max_length=400)
     
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipes")
     def __str__(self):
         return self.name 
     
--- onebowl_app/serializers.py
+++ onebowl_app/serializers.py
@@ -3,6 +3,7 @@ from .models import Recipe, Ingredient, ShoppingList
 
 from django.contrib.auth.models import User
 
+# User serializer
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)  
 
@@ -19,21 +20,29 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 
-
+# Ingredient serializer
 class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model=Ingredient
         fields="__all__"
         
+        
+        
+        
+# Recipe serializer
 # Django REST framework :https://www.django-rest-framework.org/api-guide/relations/
 # Stackoverflow: https://stackoverflow.com/questions/51182823/django-rest-framework-nested-serializers
 class RecipeSerializer(serializers.ModelSerializer):
     ingredients = IngredientSerializer(many=True, read_only=True)
+    user = serializers.PrimaryKeyRelatedField(read_only=True)  
+
     class Meta:
         model=Recipe
-        fields=['id', 'name', 'description', 'instructions', 'img_url', 'ingredients']
+        fields=['id', 'name', 'description', 'instructions', 'img_url', 'ingredients','user']
         
         
+        
+# Shopping List serializer  
 class ShoppingListSerializer(serializers.ModelSerializer):
     class Meta:
         model=ShoppingList
