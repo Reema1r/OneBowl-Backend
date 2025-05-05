abaa1e4c68915c525ce66eb6f5f62340d7ff961f
Reema Radi <reema1radi1@outlook.com>
2025-05-05 22:09:52 +0300

    added owner field in recipe model

--- onebowl_app/models.py
+++ onebowl_app/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Recipe(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=1000)
     instructions = models.TextField()
     img_url = models.CharField(max_length=400)
-    
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes')
+
     def __str__(self):
         return self.name 
     
--- onebowl_project/settings.py
+++ onebowl_project/settings.py
@@ -31,8 +31,17 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
-# Application definition
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
+# Application definition
 INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
