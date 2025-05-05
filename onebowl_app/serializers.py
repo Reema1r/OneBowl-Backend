d5b8b8fd6a15c979c8c3a4ba71e738328d5da3e1
Reema Radi <reema1radi1@outlook.com>
2025-05-05 16:24:06 +0300

    added user serializer

--- onebowl_app/serializers.py
+++ onebowl_app/serializers.py
@@ -1,12 +1,29 @@
 from rest_framework import serializers
 from .models import Recipe, Ingredient, ShoppingList
 
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)  
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+        
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']  )
+        return user
+
+
+
 
 class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model=Ingredient
         fields="__all__"
-        
         
 # Django REST framework :https://www.django-rest-framework.org/api-guide/relations/
 # Stackoverflow: https://stackoverflow.com/questions/51182823/django-rest-framework-nested-serializers
